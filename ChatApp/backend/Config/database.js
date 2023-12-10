8cfe7d1313e7ef7a1e4763b13c01f19489d94547
242544 Szczepanek Filip <242544@edu.p.lodz.pl>
2023-12-10 19:06:56 +0100

    connection with mysql

--- ChatApp/backend/Config/database.js
+++ ChatApp/backend/Config/database.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
+const mysql = require("mysql");
 
 const dataBase = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
-
-    console.log(`Mongoo database is connected: ${connect.connection.host}`);
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit();
-  }
+  const db = await mysql.createConnection({
+    host: "io2023.mysql.database.azure.com",
+    user: "studentuser",
+    password: "kuxgox-6zavgi-qUkkib",
+    database: "greenchallenge",
+    port: "3306",
+    ssl: true,
+  });
+  const connect = await db.connect((err) => {
+    if (err) {
+      console.error("Błąd połączenia z bazą danych:", err);
+    }
+    console.log("Mysql database is connected");
+  });
 };
 
 module.exports = dataBase;
